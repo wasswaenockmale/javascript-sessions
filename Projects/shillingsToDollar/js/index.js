56224aeb43a8047d612345924bb78abafeda3368
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-01 23:31:01 +0300

    added more values

--- Projects/shillingsToDollar/js/index.js
+++ Projects/shillingsToDollar/js/index.js
@@ -1,33 +1,20 @@
 const result = document.getElementById("res")
-const input = document.getElementById("input").value
-const reset = document.getElementById("reset")
+const input = document.getElementById("input")
+const selectFrom = document.getElementById("from_currency")
+const selectTo = document.getElementById("to_currency")
 
+
+// Creating an object that contains more the values of the different currency
+let currencyValues = {
+    dollars:3878.47,
+    euro: 3966.27
+}
 // The function that converts shillings to dollars
-let convertToDollars = (event,amount)=>{
+console.log("Things are working")
+let ShillingsToDollars = (event)=>{
     event.preventDefault()
-    result.innerHTML =  (amount/3878.47).toFixed(2)
+    // I have used an async function to wait for the input value from the user.
+    setTimeout(()=>{
+        result.innerHTML =  (input.value/3878.47).toFixed(2)
+    },50)
 }
-// The reset button should reset the h1 tag of the html
-// let resetButton = ()=>{
-//     result.innerHTML = "That was it"
-// }
-
-// // using the addEventListener()
-// reset.addEventListener('click',resetButton())
-
-// Form validation using Javascript
-function validation(){
-    let inputValue = document.forms['formname']['inputname'].value
-    if(inputValue == ""){
-        alert("Field name should not be empty.");
-        return false
-    }
-}
-
-// function questionToKevin(string){
-//     if(string === "You love me more"){
-//         console.log("I will marry you")
-//     }else{
-//         console.log("You don't wanna get married to me")
-//     }
-// }
