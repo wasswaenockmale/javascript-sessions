663d20d0c93164360f106a8bf1fc0b81636c8ab0
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-08 15:26:29 +0300

    used the fetch api to post data

--- Projects/fetchDataApi/js/post.js
+++ Projects/fetchDataApi/js/post.js
@@ -2,26 +2,31 @@ const tt = document.getElementById("title")
 const aut = document.getElementById("author")
 const bd = document.getElementById('body')
 const id = document.getElementById('numID')
-const form = document.querySelector("form")
-
+// const form = document.querySelector("form")
 let data = {}
-form.onsubmit = ()=>{
-    // Submit data here
-    data = {
-        postNumber:id.value,
-        author:aut.value,
-        title:tt.value,
-        body:bd.value
-    }
-}
-// fetch('https://kasasira.herokuapp.com/posts',{
-//     method:'post',
-//     headers:{
-//         'Content-Type':'application/json',
-//     },
-//     body: JSON.stringify(data)
-// })
-// .then((response)=>response.json())
-// .then((data)=>{
-//     console.log("successful ", data)
-// })
+const post = document.getElementById("post")
+post.addEventListener('click',(event)=>{
+    event.preventDefault()
+    data["postNumber"] = id.value
+    data["title"] = tt.value
+    data["author"] = aut.value 
+    data["body"] = bd.value
+    console.log(data)
+
+    // Using the fetch api to push and retrieve data from a given website.
+    fetch('https://kasasira.herokuapp.com/posts',{
+        method:'post',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response)=>{
+        response.json()
+        console.log(response)
+    })
+    .then((data)=>{
+        console.log("successful ", data)
+    })
+    .catch((error)=>console.error("This is an error"))
+})
