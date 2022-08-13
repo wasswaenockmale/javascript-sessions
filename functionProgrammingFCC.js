a3fac5d3655778a51dd3dcbb18f85f0b1e662fe2
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-13 10:53:48 +0300

    Added the file for functional programming sessions from freecodeCamp

---
+++ functionProgrammingFCC.js
@@ -0,0 +1,26 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(bookList, bookName) {
+  let newArr = [...bookList]
+  newArr.push(bookName);
+  return newArr;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookList, bookName) {
+  let bookarr = [...bookList]
+  const book_index = bookList.indexOf(bookName);
+  if (book_index >= 0) {
+
+    bookarr.splice(book_index, 1);
+    return bookarr;
+
+    // Change code above this line
+    }
+}
+
+console.log(add(bookList, "I love programming"))
