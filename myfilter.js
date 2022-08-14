cf68cc662e72839804b8cb9ebca3f1c4341e0f41
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-15 00:01:40 +0300

    FreecodeCamp certificate practice

---
+++ myfilter.js
@@ -0,0 +1,19 @@
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  const newArray = [];
+  for(let k = 0; k < this.length; k++){
+    // newArray.push(callback(this[k]))
+    if(callback(this[k]) == true){
+      newArray.push(this[k])
+    }
+  }
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
