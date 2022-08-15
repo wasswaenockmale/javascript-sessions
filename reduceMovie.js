e87c9d02ce6725fbed2c4d3e90ba642d5fc56944
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-15 13:40:43 +0300

    Added the urlSlug function that makes a given text input to became a url

--- reduceMovie.js
+++ reduceMovie.js
@@ -171,3 +171,11 @@ function splitify(str) {
 }
 
 splitify("Hello World,I-am code");
+
+// Only change code below this line
+function urlSlug(title) {
+  return title.toLowerCase().split(" ").filter(string=>string !== "").join("-")
+}
+// Only change code above this line
+// urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
+console.log(urlSlug(" Winter Is  Coming"))
