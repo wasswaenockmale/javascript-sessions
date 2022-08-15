134528e9fc1dd3f5bf0827e58d56507ab562ebc0
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-15 13:07:29 +0300

    Tried out the sort method in javascript

--- reduceMovie.js
+++ reduceMovie.js
@@ -136,3 +136,14 @@ const watchList = [
   const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
   // console.log(squaredIntegers);
   console.log(squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]))
+
+  function alphabeticalOrder(arr) {
+    // Only change code below this line
+  
+    return arr.sort((a,b)=>{
+      return a === b ? 0 : a > b ? 1: -1
+    })
+    // Only change code above this line
+  }
+  
+  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
