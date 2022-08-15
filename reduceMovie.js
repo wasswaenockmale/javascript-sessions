ab13be38254f617ea34675cd1b43fe4364a8a8b1
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-15 13:06:56 +0300

    Added a square list method

--- reduceMovie.js
+++ reduceMovie.js
@@ -126,4 +126,13 @@ const watchList = [
     return averageRating;
   }
   
-  console.log(getRating(watchList));
+  // console.log(getRating(watchList));
+  const squareList = arr => {
+    // Only change code below this line
+    return arr.filter(element =>element>=0 && (element - (Math.floor(element))) === 0).map(element=>element*element);
+    // Only change code above this line
+  };
+  
+  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+  // console.log(squaredIntegers);
+  console.log(squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]))
