1ceba147646b9bb2dcb434fa836c145e733417c6
Wasswa Enock Male <wasswaenockmale@gmail.com>
2022-08-15 13:54:59 +0300

    Learned how to curry function and how to do partial curring

--- reduceMovie.js
+++ reduceMovie.js
@@ -198,4 +198,36 @@ function checkPositive(arr) {
   // Only change code above this line
 }
 
-checkPositive([1, 2, 3, -4, 5]);
+/**
+ * Introduction to Currying and Partial Application
+ * The arity of a function is the number of arguments it requires. 
+ * Currying a function means to convert a function of N arity into N functions of arity 1.
+ * In other words, it restructures a function so it takes one argument, 
+ * then returns another function that takes the next argument, and so on.
+ * Here's an example:
+ * @param {*} x 
+ * @returns 
+ */
+function add(x) {
+  // Only change code below this line
+  return function(y){
+    return function(z){
+      return x + y + z
+    }
+  }
+  // Only change code above this line
+}
+
+console.log(add(10)(20)(30))
+
+/**
+ * Similarly, partial application can be described as applying a few arguments 
+ * to a function at a time and returning another function that is applied to more arguments. 
+ * Here's an example:
+ */
+function impartial(x, y, z) {
+  return x + y + z;
+}
+
+const partialFn = impartial.bind(this, 1, 2);
+partialFn(10); // 13
