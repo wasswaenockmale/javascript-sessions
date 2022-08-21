d1fcf4e67db065bc70ef6cac41aa71e3a8b32aa8
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 13:17:19 +0300

    Completed the search and replace string challenge

--- intermediateScripting/SearchAndReplace.js
+++ intermediateScripting/SearchAndReplace.js
@@ -1,5 +1,12 @@
 function myReplace(str, before, after) {
-    return str;
+    // let capital = /[A-Z]/
+    // if(capital.test(before)){
+    //     after = after[0].toUpperCase() + after.slice(1)
+    // }
+
+    // return str.replace(before, after)
+    return /[A-Z]/.test(before[0])?str.replace(before, after[0].toUpperCase()+after.slice(1)):str.replace(before, after.toLowerCase());
   }
   
-  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"))
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"))
