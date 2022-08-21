ba3ff1176b87160db4499b428bfe10d3bb37479c
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 10:28:51 +0300

    Removed everything in the code

--- intermediateScripting/SpinalTapCase.js
+++ intermediateScripting/SpinalTapCase.js
@@ -1,9 +1,5 @@
 function spinalCase(str) {
-    if((/\s/).test(str)){
-        return str.toLowerCase().split(" ").join("-");
-    }else{
-        
-    }
-  }
+
+}
   
 console.log(spinalCase('This Is Spinal Tap'))
