6e93e77e7f2e4272f8754ae6ea1c6f5f9551100a
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 23:57:31 +0300

    learned how to use the every() method of an array

---
+++ intermediateScripting/SumPrime.js
@@ -0,0 +1,12 @@
+function sumPrimes(num) {
+    let primes = []
+    for(let i = 2; i < num+1; i++){
+        if(primes.every((prime)=> i % prime !== 0)){
+            primes.push(i)
+        }
+    }
+    return primes.reduce((sum, prime)=>sum + prime,0)
+}
+  
+console.log(sumPrimes(10))
+console.log(sumPrimes(977))
