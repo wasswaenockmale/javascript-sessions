4372e13e71f38dd5e8522a271b0f60597f75f0eb
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 09:54:22 +0300

    whereforeArtThou challenge from freecodeCamp

--- intermediateScripting/WhereforeArtThou.js
+++ intermediateScripting/WhereforeArtThou.js
@@ -2,16 +2,16 @@ function whatIsInAName(collection, source) {
     let arr = [];
     // Only change code below this line
     collection.forEach(element=>{
-      if(element.size === source.length){
-        for(let src in source){
-            if(element[src] === source[src]){
-                arr.push(element)
-                break
-            }
-          }
+      for(let k in source){
+        if(element[k] === source[k]){
+          // console.log(element)
+          arr.push(element)
+        }
       }
     })
     // Only change code above this line
     return arr;
   }
   
+  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
+  console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }))
