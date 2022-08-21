6afd5d1f4374e851f86e2dba88aac93e9d23e8b7
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 11:04:06 +0300

    Completed the SpinalTapCase challenge

--- intermediateScripting/SpinalTapCase.js
+++ intermediateScripting/SpinalTapCase.js
@@ -1,5 +1,25 @@
 function spinalCase(str) {
+    let nonNumeric = /[^a-zA-Z\d:]/g
+    let capitalLetter = /[A-Z]/g
+    let newStr = str.replace(nonNumeric, "")
 
+    let res = ""
+    let count = 0
+    for(let k of newStr){
+        if(!capitalLetter.test(k)){
+            res += k
+        }else{
+            if(count === 0){
+                res += k
+                count++
+            }else{
+                res += " " + k
+                count++
+            }
+        }
+    }
+    return res.split(" ").join("-").toLowerCase()
 }
-  
-console.log(spinalCase('This Is Spinal Tap'))
+
+console.log(spinalCase("This Is Spinal Tap"))
+console.log(spinalCase("The_Andy_Griffith_Show"))
