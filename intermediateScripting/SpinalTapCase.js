87c273e58d0d9a7c90eb99a3cf145a54738730e5
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 10:27:59 +0300

    Checking whether the string has a space character

--- intermediateScripting/SpinalTapCase.js
+++ intermediateScripting/SpinalTapCase.js
@@ -1,5 +1,9 @@
 function spinalCase(str) {
-    return str;
+    if((/\s/).test(str)){
+        return str.toLowerCase().split(" ").join("-");
+    }else{
+        
+    }
   }
   
 console.log(spinalCase('This Is Spinal Tap'))
