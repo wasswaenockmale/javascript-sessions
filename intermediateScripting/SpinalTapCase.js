cefa32c1a9bac07eee9674e013827b8f316a1983
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 11:51:39 +0300

    Completed the spinal tap case challenge from freeCodeCamp

--- intermediateScripting/SpinalTapCase.js
+++ intermediateScripting/SpinalTapCase.js
@@ -1,25 +1,32 @@
 function spinalCase(str) {
-    let nonNumeric = /[^a-zA-Z\d:]/g
+    let nonNumeric = /[^a-zA-Z\d\s:-]/g
     let capitalLetter = /[A-Z]/g
-    let newStr = str.replace(nonNumeric, "")
+    let newStr = str.replace(nonNumeric, "").replace(/\s/g, "-")
 
     let res = ""
-    let count = 0
-    for(let k of newStr){
-        if(!capitalLetter.test(k)){
-            res += k
+    // console.log(newStr)
+    for(let k = 0; k < newStr.length; k++){
+        if(k === 0){
+            res += newStr[k]
         }else{
-            if(count === 0){
-                res += k
-                count++
+            if(capitalLetter.test(newStr[k])){
+                if(newStr[k] === "-"){
+                    res += newStr[k]
+                }else{
+                    if(newStr[k-1] === "-"){
+                        res += newStr[k]
+                    }else{
+                        res += "-" + newStr[k]
+                    }
+                }
             }else{
-                res += " " + k
-                count++
+                res += newStr[k]
             }
         }
     }
-    return res.split(" ").join("-").toLowerCase()
+    return res.toLowerCase()
 }
 
 console.log(spinalCase("This Is Spinal Tap"))
 console.log(spinalCase("The_Andy_Griffith_Show"))
+console.log(spinalCase("Teletubbies say Eh-oh"))
