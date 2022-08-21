1ca932be9bc06cb417cd026a6395118b70fe1781
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 10:07:10 +0300

    Added new test cases

--- intermediateScripting/WhereforeArtThou.js
+++ intermediateScripting/WhereforeArtThou.js
@@ -24,3 +24,5 @@ function whatIsInAName(collection, source) {
   
   console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
   console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }))
+  // whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
+  console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }))
