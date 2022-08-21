a2f41ae13e14a75730ba4781f509df89287cd819
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 21:35:39 +0300

    completed the sumAllFibbNum challenge from freeCodeCamp

--- intermediateScripting/SumAllFibbNum.js
+++ intermediateScripting/SumAllFibbNum.js
@@ -1,5 +1,23 @@
 function sumFibs(num) {
-    return num;
+    let fib = [1]
+    let sum = 0
+    for(let i = 0; i <= num; i++){
+        if(fib[i] < num){
+            if(fib.length === 1){
+                fib.push(1)
+            }else{
+                fib.push(fib[i -1] + fib[i])
+            }
+        }
+    }
+
+    for(let k of fib){
+        if(k <= num && k%2 !== 0){
+            sum += k
+        }
+    }
+    return sum
   }
   
-  sumFibs(4);
+  console.log(sumFibs(4))
+  console.log(sumFibs(4000000))
