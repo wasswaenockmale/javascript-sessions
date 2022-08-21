83f3fa141ba842a4bbfe4617529e963e81ee1bec
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 16:51:43 +0300

    Completed the sorted Union challenge from freeCodeCamp

--- intermediateScripting/SortedUnion.js
+++ intermediateScripting/SortedUnion.js
@@ -1,5 +1,15 @@
 function uniteUnique(arr) {
-    return arr;
+    let res = []
+    let ar = [...arguments]
+    for(let k = 0; k < ar.length; k++){
+        // console.log(ar[k])
+        for(let m = 0; m < ar[k].length; m++){
+            if(res.indexOf(ar[k][m]) < 0){
+                res.push(ar[k][m])
+            }
+        }
+    }
+    return res;
   }
   
-  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+ console.log( uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
