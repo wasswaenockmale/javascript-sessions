c81283c2c28587db8bafe7b6705c4e2edd55cc5b
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 16:11:57 +0300

    Learned how to use string.charCodeAt() and String.fromCharCode()

--- intermediateScripting/MissingLetter.js
+++ intermediateScripting/MissingLetter.js
@@ -1,5 +1,18 @@
 function fearNotLetter(str) {
-    return str;
+    let len =  str.length
+    let letter
+    for(let k = 0; k < len; k++){
+        // console.log(code)
+        if(k === 0){
+            continue
+        }else{
+            let code = str[k].charCodeAt(str[k])
+            if(code - (str[k-1].charCodeAt(str[k-1])) > 1){
+                letter = String.fromCharCode(code-1)
+            }
+        }
+    }
+    return letter;
   }
   
-  fearNotLetter("abce");
+console.log(fearNotLetter("abce"))
