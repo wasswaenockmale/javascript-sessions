be8578dd79f7d4fb7bbb7a8ebd494be7155b0a0c
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-21 21:39:57 +0300

    Added the simpler solution of the freeCodeCamp

--- intermediateScripting/SumAllFibbNum.js
+++ intermediateScripting/SumAllFibbNum.js
@@ -21,3 +21,19 @@ function sumFibs(num) {
   
   console.log(sumFibs(4))
   console.log(sumFibs(4000000))
+
+
+//   function sumFibs(num){
+//     let prevNumber = 0;
+//     let currNumber = 1;
+//     let result = 0;
+//     while (currNumber <= num) {
+//       if (currNumber % 2 !== 0) {
+//         result += currNumber;
+//       }
+//       currNumber += prevNumber;
+//       prevNumber = currNumber - prevNumber;
+//     }
+  
+//     return result;
+//   }
