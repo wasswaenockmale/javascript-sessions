ea4c6df2984b56790049a1c3b3eefb0ea29962aa
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-22 17:28:41 +0300

    Completed the challenge from FreeCodeCamp

--- intermediateScripting/ArgumentOption.js
+++ intermediateScripting/ArgumentOption.js
@@ -1,5 +1,22 @@
 function addTogether() {
-    return false;
+    for(let k of arguments){
+        if(typeof k !== "number"){
+            return
+        }
+    }
+    let k = [...arguments][0]
+    if(arguments.length>1){
+        return [...arguments].reduce((sum, currentV)=> { return sum += currentV},0)
+    }else{
+
+        return function(z){
+            if(typeof z !== "number"){
+                return
+            }else{
+                return z + k
+            }
+        }
+    }
   }
   
-  addTogether(2,3);
+  console.log(addTogether(10)([10]))
