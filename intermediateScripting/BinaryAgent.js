bed6bbb75f52f78bc46b5e6dab368a62c2299695
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-22 13:12:29 +0300

    Learned how to use the map() method of the arrays in javascript

--- intermediateScripting/BinaryAgent.js
+++ intermediateScripting/BinaryAgent.js
@@ -1,5 +1,14 @@
 function binaryAgent(str) {
-    return str;
+    // console.log(String.fromCharCode(str.split(" ")[0]))
+    // let arr = []
+    // let string = str.split(" ")
+    // let len = string.length
+    // for(let k = 0; k < len; k++){
+    //     arr.push(String.fromCharCode(parseInt(string[k],2)))
+    // }
+
+    // return arr.join("")
+    return str.split(" ").map(element => String.fromCharCode(parseInt(element,2))).join("")
 }
   
-binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
