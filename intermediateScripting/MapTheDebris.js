3bf9bcd314cbc9c06bcd34e66924e4cf6a37ec06
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-22 18:23:27 +0300

    New working code

--- intermediateScripting/MapTheDebris.js
+++ intermediateScripting/MapTheDebris.js
@@ -7,5 +7,11 @@ function orbitalPeriod(arr) {
     });
   }
   
+
+//   return arr.map(({ name, avgAlt }) => {
+//     const earth = earthRadius + avgAlt;
+//     const orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earth, 3)/GM));
+//     return { name, orbitalPeriod };
+//   });
   console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]))
   console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]))
