593d9f0c24b812ac34dc8a11ae23a1f523779e9d
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-22 12:21:56 +0300

    Completed the drop it challenge from freeCodeCamp

--- intermediateScripting/DropIt.js
+++ intermediateScripting/DropIt.js
@@ -1,5 +1,15 @@
 function dropElements(arr, func) {
-    return arr;
-  }
+    let len = arr.length
+    for(let k = 0; k < len; k++){
+        if(func(arr[0])){
+            break
+        }else{
+            arr.shift()
+        }
+    }
+    return arr
+}
   
-  dropElements([1, 2, 3], function(n) {return n < 3; });
+console.log(dropElements([1, 2, 3], function(n) {return n < 3; }))
+// console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}))
+// console.log(dropElements(dropElements([1, 2, 3, 4], function(n) {return n > 5;})))
