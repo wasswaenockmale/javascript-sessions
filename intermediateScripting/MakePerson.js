17429e684ed127ee8924c95a3de3960b39c2a558
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-22 17:56:37 +0300

    completed the make person challenge

--- intermediateScripting/MakePerson.js
+++ intermediateScripting/MakePerson.js
@@ -1,25 +1,32 @@
 const Person = function(firstAndLast) {
     // Only change code below this line
-    // Complete the method below and implement the others similarly
+    // Complete the method below and implement the others similarl
+    let name = firstAndLast
     this.getFullName = function() {
-      return firstAndLast;
+      return name;
     }
     this.getFirstName = function(){
-      return firstAndLast.split(" ")[0]
+      return name.split(" ")[0]
     }
     this.getLastName = function(){
-      return firstAndLast.split(" ")[1]
+      return name.split(" ")[1]
     }
     this.setFirstName = function(first){
-      
+        name = first +" "+name.split(" ")[1]
     }
     this.setLastName = function(last){
-  
+        name = name.split(" ")[0]+" "+last
     }
-    this.setFullName = function(firstAndLast){
-      
+    this.setFullName = function(another){
+      name = another
     }
   };
   
-  const bob = new Person('Bob Ross');
-  bob.getFullName();
+  const bob = new Person('Bob Ross')
+  console.log(bob.getFullName())
+  console.log(bob.getFirstName())
+  console.log(bob.getLastName())
+  bob.setFirstName("Enock")
+  bob.setFullName("Enock Wasswa")
+  bob.setLastName("Male")
+  console.log(bob.getFullName())
