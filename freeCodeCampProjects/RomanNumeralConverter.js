a01c921572a7274282aae90bbdfdd69e35d91623
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-28 15:45:14 +0300

    finding the roman numero of a given number

--- freeCodeCampProjects/RomanNumeralConverter.js
+++ freeCodeCampProjects/RomanNumeralConverter.js
@@ -1,23 +1,53 @@
 function convertToRoman(num) {
     const roman = {
         "M":1000,
-        CM:900,
-        D:500,
-        CD:400,
-        C:100,
-        XC:90,
-        L:50,
-        XL:40,
-        X:10,
-        IX:9,
-        V:5,
-        IV:4,
-        I:1
+        "CM":900,
+        "D":500,
+        "CD":400,
+        "C":100,
+        "XC":90,
+        "L":50,
+        "XL":40,
+        "X":10,
+        "IX":9,
+        "V":5,
+        "IV":4,
+        "I":1
     }
-    let res;
+    let res = []
     let count = 0; // to count the tens and thousands.
+    while(num!=0){
+        count++;
+        let remainder = num%10
+        let prev = num;
+        if(remainder !== 0){
+            if(Object.keys(roman).find((number)=>roman[number] === remainder) !== undefined){
+                if(count > 1){
+                    prev = num + "0".repeat(count)
+                    if(Object.keys(roman).find((elem)=>roman[elem] === Number(prev)) !== undefined){
+                        // This means it actually exists.
+                        res.unshift(Object.keys(roman).find((elem)=>roman[elem] === Number(prev)))
+                    }else{
+                        // That means, it's not in the object but it's in the range of zeros
+                        
+                    }
+                }else{
+                    if(Object.keys(roman).find((elem)=>roman[elem] === Number(prev)) !== undefined){
+                        // This means it actually exists.
+                        res.unshift(Object.keys(roman).find((elem)=>{roman[elem] === Number(elem)}))
+                    }else{
+                        // That means, it's not in the object but it's in the range of zeros
+                    }
+                }
+            }else{
 
-    return Object.keys(roman).find((number)=>roman[number] === 1000);
+            }
+        }else{
+            num = num/10;
+        }
+    }
+
+    return res.join("");
 }
    
 console.log(convertToRoman(36))
