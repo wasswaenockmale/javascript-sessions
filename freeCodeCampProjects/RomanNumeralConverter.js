289c925c39ce1030ed401cb024c92a27214a350d
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-29 10:01:32 +0300

    I have included the condition for capturing the number at the begining of the file

--- freeCodeCampProjects/RomanNumeralConverter.js
+++ freeCodeCampProjects/RomanNumeralConverter.js
@@ -16,29 +16,34 @@ function convertToRoman(num) {
     }
     let res = []
     let count = 0; // to count the tens and thousands.
-    while(num !== 0){
-        count++;
-        
-        let remainder = num%10
-        let prev = num;
-        num = Math.floor(num/10)
-        // console.log(`This is the remainder: ${remainder}`)
-        let checkValue = Object.keys(roman).find((element)=>roman[element] === num)
-        let checkRemainder = Object.keys(roman).find((element)=>roman)
-        if(checkValue !== undefined){
-            res.unshift(checkValue)
-        }else{
-            prev = Number(prev + "0".repeat(count - 1))
-            let rom = Object.keys(roman).find((elem)=>roman[elem] === Number(1 + "0".repeat(count -1)))
-            if(prev > rom && prev < Number(4 + "0".repeat(count - 1))){
-                res.unshift(rom.repeat(remainder))
-            }else if(prev > Number(5 + "0".repeat(count - 1)) && prev < Number(9 + "0".repeat(count - 1))){
-                let roms = Object.keys(roman).find((elem)=>roman[elem] === Number(5 + "0".repeat(count - 1)))
-                res.unshift(roms + rom.repeat(remainder))
+    let ch = Object.keys(roman).find((elem)=>roman[elem] === num)
+    if(ch !== undefined){
+        res.unshift(ch)
+    }else{
+        while(num !== 0){
+                count++;
+                
+                let remainder = num%10
+                let prev = num;
+                num = Math.floor(num/10)
+                // console.log(`This is the remainder: ${remainder}`)
+                let checkValue = Object.keys(roman).find((element)=>roman[element] === num)
+                let checkRemainder = Object.keys(roman).find((element)=>roman)
+                if(checkValue !== undefined){
+                    res.unshift(checkValue)
+                }else{
+                    prev = Number(prev + "0".repeat(count - 1))
+                    let rom = Object.keys(roman).find((elem)=>roman[elem] === Number(1 + "0".repeat(count -1)))
+                    if(prev > rom && prev < Number(4 + "0".repeat(count - 1))){
+                        res.unshift(rom.repeat(remainder))
+                    }else if(prev > Number(5 + "0".repeat(count - 1)) && prev < Number(9 + "0".repeat(count - 1))){
+                        let roms = Object.keys(roman).find((elem)=>roman[elem] === Number(5 + "0".repeat(count - 1)))
+                        res.unshift(roms + rom.repeat(remainder))
+                    }
+                }
             }
     }
-
     return res.join("")
 }
-   
-console.log(convertToRoman(10))
+
+console.log(convertToRoman(101))
