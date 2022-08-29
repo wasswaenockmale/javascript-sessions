a455db85edad418f8828ecd280c211f02160b839
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-29 08:30:08 +0300

    removed all the other code. Trying to review what the error was

--- freeCodeCampProjects/RomanNumeralConverter.js
+++ freeCodeCampProjects/RomanNumeralConverter.js
@@ -16,38 +16,16 @@ function convertToRoman(num) {
     }
     let res = []
     let count = 0; // to count the tens and thousands.
-    while(num!=0){
+    while(num !== 0){
         count++;
         let remainder = num%10
         let prev = num;
-        if(remainder !== 0){
-            if(Object.keys(roman).find((number)=>roman[number] === remainder) !== undefined){
-                if(count > 1){
-                    prev = remainder + "0".repeat(count)
-                    if(Object.keys(roman).find((elem)=>roman[elem] === Number(prev)) !== undefined){
-                        // This means it actually exists.
-                        res.unshift(Object.keys(roman).find((elem)=>roman[elem] === Number(prev)))
-                    }else{
-                        // That means, it's not in the object but it's in the range of zeros
-                        
-                    }
-                }else{
-                    if(Object.keys(roman).find((elem)=>roman[elem] === Number(prev)) !== undefined){
-                        // This means it actually exists.
-                        res.unshift(Object.keys(roman).find((elem)=>{roman[elem] === Number(elem)}))
-                    }else{
-                        // That means, it's not in the object but it's in the range of zeros
-                    }
-                }
-            }else{
-
-            }
-        }else{
-            num = num/10;
-        }
+        num = Math.floor(num/10)
+        console.log(`This is the remainder: ${remainder}`)
+        
     }
 
-    return res.join("");
+    // return res.join("");
 }
    
-console.log(convertToRoman(36))
+console.log(convertToRoman(100))
