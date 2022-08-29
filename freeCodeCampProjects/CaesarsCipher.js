47caee54f76a6e79b158d1ee6466184c3d3537af
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-29 11:58:36 +0300

    Completed the Caesars Cipher challenge from free code camp

--- freeCodeCampProjects/CaesarsCipher.js
+++ freeCodeCampProjects/CaesarsCipher.js
@@ -1,5 +1,24 @@
 function rot13(str) {
-    return str;
+    const arrAlpha = ["A","B","C","D","E","F","G","H",
+        "I","J","K","L","M","N","O","P","Q",
+        "R","S","T","U","V","W","X","Y","Z"
+    ]
+    let arr = str.split("")
+    for(let k = 0; k < arr.length; k++){
+        let index = arrAlpha.indexOf(arr[k])
+        if(index !== -1){
+            let replacedIndex = 13+index
+            if(replacedIndex >= arrAlpha.length){
+                arr[k] = arrAlpha[replacedIndex - 26]
+                
+            }else{
+                arr[k] = arrAlpha[replacedIndex]
+            }
+        }
+    }
+    return arr.join("")
 }
   
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR PBQR PNZC"))
+// console.log(rot13("SERR CVMMN!"))
+// rot13("SERR PBQR PNZC")
