f3a6086305a093e81d6228a43eb67667b64b6dbe
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-29 14:27:30 +0300

    I have further made the algorithm more optimal

--- freeCodeCampProjects/RomanNumeralConverter.js
+++ freeCodeCampProjects/RomanNumeralConverter.js
@@ -35,13 +35,16 @@ function convertToRoman(num) {
                     curr = Number(curr) // Making it a number
                     if(curr > Number(1 + "0".repeat(count)) && curr < Number(5 + "0".repeat(count))){
                         res.unshift(Object.keys(roman).find((element)=> roman[element] === Number(1 + "0".repeat(count))).repeat(remainder))
+                        count++
+                        num = divisor
                     }else if(curr > Number(5 + "0".repeat(count)) && curr < Number(9 + "0".repeat(count))){
                         let firstValue = Object.keys(roman).find((element)=>roman[element] === Number(5 + "0".repeat(count)))
                         let last = Object.keys(roman).find((element)=>roman[element] === Number(1 + "0".repeat(count))).repeat(remainder - 5)
                         res.unshift(firstValue + last)
+                        count++
+                        num = divisor
                     }
-                    count++
-                    num = divisor
+                    
                 }
 
                 
@@ -51,10 +54,11 @@ function convertToRoman(num) {
 }
 
 // console.log(convertToRoman(1111))
-console.log(convertToRoman(83))
-console.log(convertToRoman(99))
-console.log(convertToRoman(97))
+// console.log(convertToRoman(83))
+// console.log(convertToRoman(99))
+// console.log(convertToRoman(97))
+// // console.log(convertToRoman(68))
 // console.log(convertToRoman(68))
-console.log(convertToRoman(68))
-console.log(convertToRoman(2014))
-console.log(convertToRoman(1023))
+// console.log(convertToRoman(2014))
+// console.log(convertToRoman(1023))
+console.log(convertToRoman(797))
