14018cbe81e99b4bd800694dd687b4f614df05d1
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-31 19:56:20 +0300

    Trying out nested promises in javascript

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,19 +1,29 @@
 let empty_tank = function(){
-    return new Promise(function(){
+    return new Promise(function(resolve, reject){
         resolve("The tank is empty")
     })
 }
 
 let engine = function(){
-    return new Promise(function(){
+    return new Promise(function(resolve, reject){
         resolve("The engine is over heating.")
     })
 }
 
 let travel = function(){
-    return new Promise(function(){
+    return new Promise(function(resolve, reject){
         resolve("The car is not safe for travelling.")
     })
 }
 
 
+empty_tank()
+.then((val)=>{
+    console.log("This is the first value")
+    return engine()
+}).then((val)=>{
+    console.log("This is the first value")
+    return travel()
+}).then(()=>{
+    console.log("This is done")
+})
