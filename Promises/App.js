b01deaa56e590916025553c585fe19145f4ba670
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-31 17:23:09 +0300

    I have designed the resolve and reject functions in the custom promise that I have designed

--- Promises/App.js
+++ Promises/App.js
@@ -2,9 +2,29 @@ const PENDING = 0;
 const FULLFILLED = 1;
 const REJECTED = 2;
 
-function myPromiseDesign(resolve, reject){
+function myPromiseDesign(res, rej){
     let value = null;
     let state = PENDING;
     let handlers = []
     let catches = []
+
+    function resolve(v){
+        if(state !== PENDING) return;
+
+        state = FULLFILLED;
+        value = v
+
+        handlers.forEach(handler=>handler(value))
+    }
+
+    function reject(er){
+        if(state !== PENDING) return;
+
+        state = REJECTED;
+        value = er
+
+        catches.forEach(c=>c(value))
+    }
+
+    
 }
