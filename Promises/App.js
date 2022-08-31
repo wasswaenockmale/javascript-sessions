c5da36a2115699cf21d681f8300a425945d1e46b
Wasswa Enock Male <enockmale97@gmail.com>
2022-08-31 11:23:46 +0300

    I have added code to implement the promise theory

--- Promises/App.js
+++ Promises/App.js
@@ -2,3 +2,20 @@ const FULLFILLED = 1;
 const REJECTED = 2;
 const PENDING = 0;
 
+function customPromise(executor){
+    let state = PENDING;
+    let value = null;
+    let handlers = [];
+    let catches = [];
+
+    function resolve(result){
+        if(state !== PENDING) return;
+
+        state = FULLFILLED;
+        value = result;
+    }
+
+    function reject(){
+        
+    }
+}
