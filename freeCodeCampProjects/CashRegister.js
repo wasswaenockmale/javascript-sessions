ae456939307760aa928b27e309bc414c58da1f4b
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-01 15:46:01 +0300

    had to multiply the values of the money with 100

--- freeCodeCampProjects/CashRegister.js
+++ freeCodeCampProjects/CashRegister.js
@@ -42,15 +42,16 @@ function checkCashRegister(price, cash, cid) {
                     break
                 }
             }
-            console.log(sum)
+            ar[1] = value/100
             arr.push(vName, amount/100)
             
         }
 
     })
-
-      return arr
+    return arr
 }
 
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
