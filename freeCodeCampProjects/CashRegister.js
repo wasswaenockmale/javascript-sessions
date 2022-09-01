2cb3daf8b52f42f03cfc38ffea4437736908b7b8
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-01 10:12:09 +0300

    Removed the cash_in_drawer array

--- freeCodeCampProjects/CashRegister.js
+++ freeCodeCampProjects/CashRegister.js
@@ -1,16 +1,5 @@
 function checkCashRegister(price, cash, cid) {
     let change;
-    const cash_in_drawer = [
-        ["PENNY", 1.01],
-        ["NICKEL", 2.05],
-        ["DIME", 3.1],
-        ["QUARTER", 4.25],
-        ["ONE", 90],
-        ["FIVE", 55],
-        ["TEN", 20],
-        ["TWENTY", 60],
-        ["ONE HUNDRED", 100]
-      ]
     return change;
 }
   
