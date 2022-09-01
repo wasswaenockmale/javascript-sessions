7f0588a54a309ed7724656f5b5b3e2e8ada7062c
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-01 16:04:53 +0300

    calculated the finalCount to check whether money inside is enough

--- freeCodeCampProjects/CashRegister.js
+++ freeCodeCampProjects/CashRegister.js
@@ -1,7 +1,7 @@
 function checkCashRegister(price, cash, cid) {
     const change_due = (cash - price)*100
     const change_ = change_due
-    console.log(change_)
+    // console.log(change_)
     const currencyUnit = {
         "PENNY": 0.01,
         "NICKEL": 0.05,
@@ -43,15 +43,22 @@ function checkCashRegister(price, cash, cid) {
                 }
             }
             ar[1] = value/100
-            arr.push(vName, amount/100)
+            arr.push([vName, amount/100])
             
         }
-
     })
-    return arr
+
+    // This is what we shall check to see if we have enough change or not
+    let finalCount = 0;
+    arr.forEach(element=>{
+        finalCount += element[1]*100
+    })
+    
+    
+    // return totalcid === change_due/100 ? {"status":"CLOSED", "change": cid} : 
 }
 
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
