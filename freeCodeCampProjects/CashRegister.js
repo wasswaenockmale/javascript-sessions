0de879a8671d554f0b894987430d87685b4918af
Wasswa Enock Male <enockmale97@gmail.com>
2022-09-01 11:25:37 +0300

    I have added the currencyUnit to the file

--- freeCodeCampProjects/CashRegister.js
+++ freeCodeCampProjects/CashRegister.js
@@ -1,6 +1,21 @@
 function checkCashRegister(price, cash, cid) {
-    let change;
-    return change;
+    let change_due = cash - price
+    let arr = [];
+    let count = 0;
+    let change = 0;
+
+    const currencyUnit = {
+        "PENNY": 0.01,
+        "NICKEL": 0.05,
+        "DIME": 0.1,
+        "QUARTER": 0.25,
+        "ONE": 1,
+        "FIVE": 5,
+        "TEN": 10,
+        "TWENTY": 20,
+        "ONE HUNDRED": 100
+      }
 }
-  
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
